f851409841f37cf481640f3c42d5cc438935a954
Emilien Bresson <emilien.bresson@gmail.com>
2023-01-17 21:46:18 +0100

    Commit initial

---
+++ app/src/main/java/com/rga25/easyts2android/MainActivity.java
@@ -0,0 +1,24 @@
+package com.rga25.easyts2android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void ButtonOpenFile_OnClick(View view)
+    {
+      ;
+    }
+}
